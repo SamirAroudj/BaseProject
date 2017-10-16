5a0f6a46336dad05747dc228eca91ac71a453df6
Samir Aroudj <samir.aroudj@gris.informatik.tu-darmstadt.de>
2017-10-16 16:08:08 +0200

    adapted default paths for tinyxml2

--- CMake/TinyXML2.h.cmake
+++ CMake/TinyXML2.h.cmake
@@ -9,9 +9,9 @@
 # TinyXML2 specific cmake functionality
 
 # cached path to external TinyXML2 project include directory
-set(TINYXML2_INCLUDE_DIR "../TinyXML2" CACHE PATH "Path to TinyXML2 root directory containing tinyxml2.h.")
-set(TINYXML2_DEBUG_LIBRARY "../TinyXML2/Debug/TinyXML2Lib" CACHE FILEPATH "Complete path and name of TinyXML2 library file.")
-set(TINYXML2_RELEASE_LIBRARY "../TinyXML2/Release/TinyXML2Lib" CACHE FILEPATH "Complete path and name of TinyXML2 library file.")
+set(TINYXML2_INCLUDE_DIR "../tinyxml2" CACHE PATH "Path to TinyXML2 root directory containing tinyxml2.h.")
+set(TINYXML2_DEBUG_LIBRARY "../tinyxml2/Build/Debug/libtinyxml2.so" CACHE FILEPATH "Complete path and name of TinyXML2 library file.")
+set(TINYXML2_RELEASE_LIBRARY "../tinyxml2/Build/Release/libtinyxml2.so" CACHE FILEPATH "Complete path and name of TinyXML2 library file.")
 
 # make include directory available
 include_directories(${TINYXML2_INCLUDE_DIR})
